5138dcfc48ebe7eec897af41ceaa401535c668b1
Yamilexkiara <yamilexo1999@gmail.com>
2022-07-18 17:46:21 -0400

    moved bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Searchbar from "./Searchbar";
 import Temps from "./Temps";
 import WeatherNow from "./WeatherNow";
