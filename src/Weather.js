989ea3a74f612571df16ff5c6a100777aacaadf5
Yamilexkiara <yamilexo1999@gmail.com>
2022-07-26 15:03:22 -0400

    fixing bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,10 @@
 import React from "react";
 import axios from "axios";
-export default function Weather(props) {
+export default function weather(props) {
   function handleResponse(response) {
     alert(
       `The weather in ${response.data.name} is ${response.data.main.temp}° F`
-    );
+    )
   }
   let apiKey = "bd3bb6534458ba51b48c49f5155745b6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}kk&appid=${apiKey}&units=imperial`;
